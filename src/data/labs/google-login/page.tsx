182fef83fdcf8b8c312fc1500f34372056480413
erian <oops.erian@gmail.com>
2025-01-09 10:46:06 +0800

    chore:login lab

--- src/data/labs/google-login/page.tsx
+++ src/data/labs/google-login/page.tsx
@@ -1,23 +1,29 @@
+import { useState } from "react";
 import "./style.css";
-import { IconAt,IconKey } from "@tabler/icons-react";
+import { IconAt, IconKey } from "@tabler/icons-react";
+import { cn } from "@/lib/utils";
 
 export default function GoogleLogin() {
+    const [error, setError] = useState(false);
     return (
         <div className="google-login-wrap">
             <div className="sign-wrap">
                 <form>
                     <div className="field-wrap">
                         <div className="input-wrap">
-                            <input id="account" placeholder="account" type="text" />
-                            <IconAt/>
+                            <input autoComplete="off" id="account" placeholder="account" type="text" />
+                            <IconAt />
                         </div>
                         <span className="split-line"></span>
-                        <div className="input-wrap">
-                            <input id="password" placeholder="password" type="password" />
-                            <IconKey/>
+                        <div className={cn(
+                            'input-wrap',
+                            { 'error': error }
+                        )}>
+                            <input autoComplete="off" onInput={() => setError(false)} id="password" placeholder="password" type="password" />
+                            <IconKey />
                         </div>
                     </div>
-                    <button type="button">Sign in</button>
+                    <button onClick={() => setError(true)} type="button">Sign in</button>
                 </form>
             </div>
         </div>
