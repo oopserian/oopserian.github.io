291835b6429a2f33b04678751eaab0028369166c
oopserian <oopserian@users.noreply.github.com>
2025-01-14 06:52:54 +0000

    chore:adjust a few data b9e61872b2d8c5a996dba3c6d48bfb0adab542f5

--- assets/page-C38da6Hu.js
+++ assets/page-C38da6Hu.js
@@ -1,4 +1,4 @@
-import{a as s,r as l,j as a,c as o}from"./index-DBdn1v_v.js";/**
+import{a as s,r as l,j as a,c as o}from"./index-DtYeGO-j.js";/**
  * @license @tabler/icons-react v3.17.0 - MIT
  *
  * This source code is licensed under the MIT license.
